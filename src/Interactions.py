77545420e25142f8a3e0135ca8cf6e102d8539a3
lwcreel <landon.creel@icloud.com>
2020-11-10 17:23:45 -0600

    finished queue boat interaction

--- src/Interactions.py
+++ src/Interactions.py
@@ -1,5 +1,8 @@
 import sqlite3
 
+from datetime import datetime
+from random import randrange
+
 # script for login 
 def login(email, pwd):
 
@@ -8,9 +11,36 @@ def login(email, pwd):
     c  = db.cursor()
 
     # attempt to fetch corresponding User
-    c.execute('SELECT Users from dry_dock WHERE email = "%s" AND password="%s"' %(email, pwd))
+    c.execute('SELECT Users from dry_dock WHERE email=? AND password=?', email, pwd)
     if c.fetchone() is not None:
         return "Welcome"
     else:
         return "Login failed"
+
+def queueBoat(boatID, ownerID, timeRequested):
+    # connect to database
+    db = sqlite3.connect('../data/dry_dock.db')
+    c  = db.cursor()
+    rID  = randrange(10000000)
+
+    c.execute('Select Users from dry_dock WHERE user_id=?', ownerID)
+
+    if c.fetchone is not None:
+        
+        # create row in request table
+        c.execute('INSERT INTO Requests VALUES (?,?,?,?)', (rID, boatID, timeRequested, 0))
+
+        # update user with request number
+        c.execute('UPDATE Users SET request_id=? WHERE user_id=?', rID, ownerID)
+
+        return "Your request has been added to the queue! Request ID: " + str(rID)
+    else:
+        return "ERROR: user not known"
+
+    
+
+
+def dequeueBoat(boatID, requestID):
+    pass
+
 
